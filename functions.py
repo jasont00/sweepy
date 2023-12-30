e0a94e8f1f4ef09a36b23d759bd0937344bfb383
Jason <91354148+jasont00@users.noreply.github.com>
2023-12-29 21:52:49 -0500

    added new functions + bugfixes
    added "all" version of former functions which prints out all times up to 10.5 hours and fixed a bug where the outputted time would incorrectly display the am/pm.

--- functions.py
+++ functions.py
@@ -1,23 +1,26 @@
+#utility functions for sweepy.py
+
+
 # returns a string with the current time
 def currTime(hour: int, minute: int, am_pm: str):
-  out_str = "Current time is: "
+  out_str = "Input time is: "
   return out_str + timeStr(hour, minute, am_pm)
+
 
 # returns a string with the wake up time
 def wakeupTime(hour: int, minute: int, am_pm: str):
   out_str = "Desired wake up time is: "
   return out_str + timeStr(hour, minute, am_pm)
 
+
 # calculates the time to wake up after 6 hours
 def calcWakeUp(hour: int, minute: int, am_pm: str):
   hour = hour  # temp var
   # convert to 24 hour time
-  if (am_pm.upper() == "AM"):
-    if (hour == 12):
-      hour = 0
-  else:
-    if (hour != 12):
-      hour += 12
+  if (am_pm.upper() == "AM" and hour == 12):
+    hour = 0
+  elif (am_pm.upper() == "PM" and hour != 12):
+    hour += 12
   # convert to minutes
   total_minutes = hour * 60 + minute
   # add recommended minimum sleep time (6 hours)
@@ -27,28 +30,34 @@ def calcWakeUp(hour: int, minute: int, am_pm: str):
 
 # prints output for wake up time (6, 7.5, 9 hours of sleep)
 def printWakeUp(total_minutes: int):
-
   out_str = ""
-  am_pm = ""
   for i in range(0, 3):
     total_minutes_temp = total_minutes % (60 * 24) + 15
-    out_str += "\nYou should wake up at: "
-    am_pm = "PM" if (total_minutes_temp / 2) >= 720 else "AM"
+    out_str += "\n- You should wake up at: "
     temp = 90 * i
     total_minutes_temp += temp
-    out_str += timeStr(total_minutes_temp // 60,
-                       total_minutes_temp % 60, am_pm) + " (" + str(6+1.5*i) + " hours)"
+    out_str += timeTotStr(total_minutes_temp) + " (" + str(6 + 1.5 * i) + " hours)"
   return out_str
+
+
+def printAllWakeUp(total_minutes: int):
+  out_str = ""
+  for i in range(1, 8):
+    total_minutes_temp = total_minutes % (60 * 24) + 15
+    out_str += "\n- You should wake up at: "
+    temp = 90 * i
+    total_minutes_temp += temp
+    out_str += timeTotStr(total_minutes_temp) + " (" + str(1.5 * i) + " hours)"
+  return out_str
+
 
 def calcSleepTime(hour: int, minute: int, am_pm: str):
   hour = hour  # temp var
   # convert to 24 hour time
-  if (am_pm.upper() == "AM"):
-    if (hour == 12):
-      hour = 0
-  else:
-    if (hour != 12):
-      hour += 12
+  if (am_pm.upper() == "AM" and hour == 12):
+    hour = 0
+  elif (am_pm.upper() == "PM" and hour != 12):
+    hour += 12
   # convert to minutes
   total_minutes = hour * 60 + minute
   total_minutes -= 60 * 6
@@ -56,26 +65,58 @@ def calcSleepTime(hour: int, minute: int, am_pm: str):
     total_minutes += 60 * 24
   return total_minutes
 
+
 def printSleepTime(total_minutes: int):
   out_str = ""
-  am_pm = ""
-  for i in range (0, 3):
+  for i in range(0, 3):
     total_minutes_temp = total_minutes % (60 * 24) - 15
-    out_str += "\nYou should go to sleep at: "
+    out_str += "\n- You should go to sleep at: "
     temp = 90 * i
     total_minutes_temp -= temp
-    am_pm = "PM" if (total_minutes_temp / 2) >= 720 else "AM"
-    out_str += timeStr(total_minutes_temp // 60,
-                       total_minutes_temp % 60, am_pm) + " (" + str(6+1.5*i) + " hours)"
+    if (total_minutes_temp < 0): # if negative add 720 minutes (am to pm)
+      total_minutes_temp += (60 * 24)
+    out_str += timeTotStr(total_minutes_temp) + " (" + str(6 + 1.5 * i) + " hours)"
   return out_str
+
+
+def printAllSleepTime(total_minutes: int):
+  out_str = ""
+  for i in range(1, 8):
+    total_minutes_temp = (total_minutes % (60 * 24)) - 15
+    out_str += "\n- You should go to sleep at: "
+    temp = 90 * i
+    total_minutes_temp -= temp # decrement
+    if (total_minutes_temp < 0): # if negative add 720 minutes (am to pm)
+      total_minutes_temp += (60 * 24)
+    out_str += timeTotStr(total_minutes_temp) + " (" + str(1.5 * i) + " hours)"
+  return out_str
+
 
 def timeStr(hour: int, minute: int, am_pm: str):
   out_str = ""
-  if (hour%24 >= 12): # handles case where hours are greater than 12
-    am_pm = "PM"
-  #hour
-  hour_str = str(hour%24%12)
+
+  hour_str = str(hour) if hour <= 12 else str(hour % 12)
+
   out_str += hour_str + ":"  # hour
+  if (minute < 10):  # minutes
+    out_str += "0" + str(minute)
+  else:
+    out_str += str(minute)
+  out_str += " " + am_pm.upper()
+  return out_str
+
+def timeTotStr(totalMin: int):
+  out_str = ""
+  if (totalMin > 60 * 24):
+    totalMin = totalMin % (60 * 24)
+  hour = totalMin // 60
+  minute = totalMin % 60
+  am_pm = "PM" if (totalMin >= 720) else "AM"
+  if (hour == 0):
+    hour = 12
+  if (hour > 12):
+    hour %= 12
+  out_str += str(hour) + ":"  # hour
   if (minute < 10):  # minutes
     out_str += "0" + str(minute)
   else:
--- sweepy.py
+++ sweepy.py
@@ -1,4 +1,8 @@
-# bot.py
+# sweepy.py
+# Jason Truong
+# 12/27/2023
+# A discord bot that calculates sleep times and wake up times
+
 # A file to facilitate the connection to the Discord API.
 import discord
 import os
@@ -15,7 +19,7 @@ bot = commands.Bot(command_prefix='!', intents=intents)
     name='sleep',
     help='Usage: !sleep *hour* *minute* *pm/am*.\nCalculates optimal waking up times.'
 )
-async def sleep(ctx, hour: int, minute: int, am_pm: str):
+async def sleep(ctx, hour: int, minute: int = 0, am_pm: str = "AM"):
   currTime_str = currTime(hour, minute, am_pm)
   total_minutes = calcWakeUp(hour, minute, am_pm)
   out_str = currTime_str + printWakeUp(total_minutes) + " \nThese times account for 15 minutes to fall asleep. Goodnight!"
@@ -24,16 +28,43 @@ async def sleep(ctx, hour: int, minute: int, am_pm: str):
 
 @bot.command(
     name='wakeup',
-    help='Usage: !wakeup *hour* *minute* *pm/am.\nCalculates optimal sleeping times.'
+    help='Usage: !wakeup *hour* *minute* *pm/am*.\nCalculates optimal sleeping times.'
 )
-async def wakeUp(ctx, hour: int, minute: int, am_pm: str):
+async def wakeUp(ctx, hour: int, minute: int = 0, am_pm: str = "AM"):
   wakeupTime_str = wakeupTime(hour, minute, am_pm)
   total_minutes = calcSleepTime(hour, minute, am_pm)
   out_str = wakeupTime_str + printSleepTime(total_minutes) + " \nThese times account for 15 minutes to fall asleep. Goodnight!"
   await ctx.send(out_str)
-  
 
+@bot.command(
+    name='sleepall',
+    help='Usage: !sleepall *hour* *minute* *pm/am*.\nCalculates optimal sleeping times.'
+)
+async def sleepAll(ctx, hour: int, minute: int = 0, am_pm: str = "AM"):
+  hour = hour
+  currTime_str = currTime(hour, minute, am_pm)
+  if (am_pm.upper() == "AM" and hour == 12):
+    hour = 0
+  elif (am_pm.upper() == "PM" and hour != 12):
+    hour += 12
+  total_minutes = hour * 60 + minute
+  out_str = currTime_str + printAllWakeUp(total_minutes) + " \nThese times account for 15 minutes to fall asleep. Goodnight!"
+  await ctx.send(out_str)
 
+@bot.command(
+  name='wakeupall',
+  help='Usage: !wakeup *hour* *minute* *pm/am*.\nCalculates optimal sleep sleeping times.'
+)
+async def wakeUpAll(ctx, hour: int, minute: int = 0, am_pm: str = "AM"):
+  hour = hour
+  currTime_str = currTime(hour, minute, am_pm)
+  if (am_pm.upper() == "AM" and hour == 12):
+    hour = 0
+  elif (am_pm.upper() == "PM" and hour != 12):
+    hour += 12
+  total_minutes = hour * 60 + minute
+  out_str = currTime_str + printAllSleepTime(total_minutes) + "\nThese times account for 15 minutes to fall asleep. Goodnight!"
+  await ctx.send(out_str)
 
 if TOKEN:
   bot.run(TOKEN)
